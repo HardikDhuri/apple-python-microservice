ff1ca618615393bc60c628845231bd94bb96a7ac
Hardik Dhuri <49452742+HardikDhuri@users.noreply.github.com>
2022-09-11 20:45:56 +0530

    Updated '/answer' endpoint.

--- server/routes/health.py
+++ server/routes/health.py
@@ -7,3 +7,10 @@ def health():
     """health route"""
     state = {"status": "UP"}
     return jsonify(state)
+
+@app.route("/answer")
+def answer():
+    """Answer route"""
+    state = {"Apple is red": 100}
+    return jsonify(state)
+
